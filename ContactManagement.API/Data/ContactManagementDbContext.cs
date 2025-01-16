dc278e505f21b81c5c3b699c525a22e2996d0b2f
romon1450 <romon1450@gmail.com>
2025-01-16 17:46:21 -0500

    created contact model, dbcontext, and added ef core package

---
+++ ContactManagement.API/Data/ContactManagementDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ContactManagement.API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagement.API.Data;
+
+public class ContactManagementDbContext : DbContext
+{
+    public ContactManagementDbContext(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet<Contact> Contacts { get; set; }
+}
---
+++ ContactManagement.API/Data/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactManagement.API.Data.Models;
+
+public class Contact
+{
+    public Guid Id { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+}
